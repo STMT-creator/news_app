e954979adbcc455b68df069e8d774b3a34da8a8c
stmt <elahep113@naver.com>
2025-02-18 09:44:25 +0900

    상수 이름 변경

--- newsdata.js
+++ newsdata.js
@@ -1,9 +1,9 @@
-const myNews_api = "pub_69485981d44aab754852e8f074c2963a82de1";
+const api_key = "pub_69485981d44aab754852e8f074c2963a82de1";
 const query = "pizza";
 const url = "https://newsdata.io/api/1/latest"
-const request = `${url}?apikey=${myNews_api}&q=${query}`;
+const request = `${url}?apikey=${api_key}&q=${query}`;
 
-window.addEventListener("DOMContentLoaded", function (e) {
+window.addEventListener("DOMContentLoaded", function () {
     fetchNews();
 })
 async function fetchNews() {
